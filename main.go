ae1b4961c12cbd3696700cc6987fcea8a54156ab
Sergey Gavruk <svgavruk@gmail.com>
2013-10-08 22:56:24 +0300

    add static files

--- main.go
+++ main.go
@@ -14,21 +14,18 @@ func main() {
 	app := crater.App{}
 
 	config := crater.Settings{}
-	config.ViewPath = "./Views"
+	config.ViewsPath = "./Views"
+	config.StaticFilesPath = "./Content"
 
 	app.Settings(config)
+
+	app.HandleStaticFiles("/content")
 
 	app.Get("/", func(req *crater.Request, res *crater.Response) {
 		res.Render("index", nil)
 	})
 
 	app.Get("/hello", func(req *crater.Request, res *crater.Response) {
-
-		//var username string
-		//if value, ok := req.GetString("name"); ok {
-		//	username = value
-		//}
-		//user := &models.User{Name: username}
 
 		user := &models.User{}
 
