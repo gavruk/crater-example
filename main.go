d57e753b1f8917cc215e7271dbf2f4cca7d7c4af
Sergey Gavruk <svgavruk@gmail.com>
2013-10-26 16:50:28 +0300

    use layout

--- main.go
+++ main.go
@@ -58,6 +58,10 @@ func main() {
 		res.Redirect("/signin")
 	})
 
+	app.Get("/about", func(req *crater.Request, res *crater.Response) {
+		res.Render("about", nil)
+	})
+
 	app.Get("/string", func(req *crater.Request, res *crater.Response) {
 		res.RenderString("<h1>Hello World</h1>")
 	})
